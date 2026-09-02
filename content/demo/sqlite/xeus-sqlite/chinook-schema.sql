7d82d01be3faca90314d94797c735cab669c8ec2
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2026-09-02 17:02:01 +0200

    Przewodnik XEUS-SQLite

---
+++ content/demo/sqlite/xeus-sqlite/chinook-schema.sql
@@ -0,0 +1,163 @@
+--
+-- Plik wygenerowany przez SQLiteStudio v3.4.17 dnia śr. wrz 2 14:03:14 2026
+--
+-- Użyte kodowanie tekstu: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Tabela: albums
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS "albums"
+(
+    [AlbumId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [Title] NVARCHAR(160)  NOT NULL,
+    [ArtistId] INTEGER  NOT NULL,
+    FOREIGN KEY ([ArtistId]) REFERENCES "artists" ([ArtistId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- Tabela: artists
+DROP TABLE IF EXISTS artists;
+CREATE TABLE IF NOT EXISTS "artists"
+(
+    [ArtistId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [Name] NVARCHAR(120)
+);
+
+-- Tabela: customers
+DROP TABLE IF EXISTS customers;
+CREATE TABLE IF NOT EXISTS "customers"
+(
+    [CustomerId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [FirstName] NVARCHAR(40)  NOT NULL,
+    [LastName] NVARCHAR(20)  NOT NULL,
+    [Company] NVARCHAR(80),
+    [Address] NVARCHAR(70),
+    [City] NVARCHAR(40),
+    [State] NVARCHAR(40),
+    [Country] NVARCHAR(40),
+    [PostalCode] NVARCHAR(10),
+    [Phone] NVARCHAR(24),
+    [Fax] NVARCHAR(24),
+    [Email] NVARCHAR(60)  NOT NULL,
+    [SupportRepId] INTEGER,
+    FOREIGN KEY ([SupportRepId]) REFERENCES "employees" ([EmployeeId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- Tabela: employees
+DROP TABLE IF EXISTS employees;
+CREATE TABLE IF NOT EXISTS "employees"
+(
+    [EmployeeId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [LastName] NVARCHAR(20)  NOT NULL,
+    [FirstName] NVARCHAR(20)  NOT NULL,
+    [Title] NVARCHAR(30),
+    [ReportsTo] INTEGER,
+    [BirthDate] DATETIME,
+    [HireDate] DATETIME,
+    [Address] NVARCHAR(70),
+    [City] NVARCHAR(40),
+    [State] NVARCHAR(40),
+    [Country] NVARCHAR(40),
+    [PostalCode] NVARCHAR(10),
+    [Phone] NVARCHAR(24),
+    [Fax] NVARCHAR(24),
+    [Email] NVARCHAR(60),
+    FOREIGN KEY ([ReportsTo]) REFERENCES "employees" ([EmployeeId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- Tabela: genres
+DROP TABLE IF EXISTS genres;
+CREATE TABLE IF NOT EXISTS "genres"
+(
+    [GenreId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [Name] NVARCHAR(120)
+);
+
+-- Tabela: invoice_items
+DROP TABLE IF EXISTS invoice_items;
+CREATE TABLE IF NOT EXISTS "invoice_items"
+(
+    [InvoiceLineId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [InvoiceId] INTEGER  NOT NULL,
+    [TrackId] INTEGER  NOT NULL,
+    [UnitPrice] NUMERIC(10,2)  NOT NULL,
+    [Quantity] INTEGER  NOT NULL,
+    FOREIGN KEY ([InvoiceId]) REFERENCES "invoices" ([InvoiceId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY ([TrackId]) REFERENCES "tracks" ([TrackId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- Tabela: invoices
+DROP TABLE IF EXISTS invoices;
+CREATE TABLE IF NOT EXISTS "invoices"
+(
+    [InvoiceId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [CustomerId] INTEGER  NOT NULL,
+    [InvoiceDate] DATETIME  NOT NULL,
+    [BillingAddress] NVARCHAR(70),
+    [BillingCity] NVARCHAR(40),
+    [BillingState] NVARCHAR(40),
+    [BillingCountry] NVARCHAR(40),
+    [BillingPostalCode] NVARCHAR(10),
+    [Total] NUMERIC(10,2)  NOT NULL,
+    FOREIGN KEY ([CustomerId]) REFERENCES "customers" ([CustomerId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- Tabela: media_types
+DROP TABLE IF EXISTS media_types;
+CREATE TABLE IF NOT EXISTS "media_types"
+(
+    [MediaTypeId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [Name] NVARCHAR(120)
+);
+
+-- Tabela: playlist_track
+DROP TABLE IF EXISTS playlist_track;
+CREATE TABLE IF NOT EXISTS "playlist_track"
+(
+    [PlaylistId] INTEGER  NOT NULL,
+    [TrackId] INTEGER  NOT NULL,
+    CONSTRAINT [PK_PlaylistTrack] PRIMARY KEY  ([PlaylistId], [TrackId]),
+    FOREIGN KEY ([PlaylistId]) REFERENCES "playlists" ([PlaylistId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY ([TrackId]) REFERENCES "tracks" ([TrackId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+-- Tabela: playlists
+DROP TABLE IF EXISTS playlists;
+CREATE TABLE IF NOT EXISTS "playlists"
+(
+    [PlaylistId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [Name] NVARCHAR(120)
+);
+
+-- Tabela: tracks
+DROP TABLE IF EXISTS tracks;
+CREATE TABLE IF NOT EXISTS "tracks"
+(
+    [TrackId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    [Name] NVARCHAR(200)  NOT NULL,
+    [AlbumId] INTEGER,
+    [MediaTypeId] INTEGER  NOT NULL,
+    [GenreId] INTEGER,
+    [Composer] NVARCHAR(220),
+    [Milliseconds] INTEGER  NOT NULL,
+    [Bytes] INTEGER,
+    [UnitPrice] NUMERIC(10,2)  NOT NULL,
+    FOREIGN KEY ([AlbumId]) REFERENCES "albums" ([AlbumId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY ([GenreId]) REFERENCES "genres" ([GenreId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY ([MediaTypeId]) REFERENCES "media_types" ([MediaTypeId]) 
+		ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
